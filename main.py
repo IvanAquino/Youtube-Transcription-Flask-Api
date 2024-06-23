66484f01398e612a5692c195d4ee843b5eea75fb
Ivan Aquino <ivan@codigoaxaca.com>
2024-06-23 11:58:29 -0600

    Added error handling and readme

--- main.py
+++ main.py
@@ -14,23 +14,22 @@ def get_transcript(video_id):
             try:
                 return transcript.fetch()
             except Exception as e:
-                print(f"Error fetching transcript in {transcript.language}: {e}")
+                raise Exception(f"Error fetching transcript in {transcript.language}: {e}")
     except TranscriptsDisabled:
-        print("Transcripts are disabled for this video.")
+        raise Exception("Transcripts are disabled for this video.")
     except NoTranscriptFound:
-        print("No transcript found for this video.")
+        raise Exception("No transcript found for this video.")
     except Exception as e:
-        print(f"Error fetching transcripts: {e}")
-    return []
+        raise Exception(f"Error fetching transcripts: {e}")
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
-            return jsonify({"error": "Token is missing!"}), 401
+            return jsonify({"message": "Token is missing!"}), 401
         if token != AUTH_TOKEN:
-            return jsonify({"error": "Invalid token!"}), 403
+            return jsonify({"message": "Invalid token!"}), 403
         return f(*args, **kwargs)
     return decorated
 
@@ -39,11 +38,11 @@ def token_required(f):
 def transcript_text():
     video_id = request.args.get('video_id')
     if not video_id:
-        return "Please provide a video ID.", 400
+        return jsonify({"message": "Please provide a video ID."}), 400
     
     transcript = get_transcript(video_id)
     if not transcript:
-        return "Unable to fetch transcript.", 404
+        return jsonify({"message": "Unable to fetch transcript."}), 404
 
     transcript_text = "\n".join([entry['text'] for entry in transcript])
     return transcript_text
@@ -53,13 +52,19 @@ def transcript_text():
 def transcript_json():
     video_id = request.args.get('video_id')
     if not video_id:
-        return jsonify({"error": "Please provide a video ID."}), 400
+        return jsonify({"message": "Please provide a video ID."}), 400
 
     transcript = get_transcript(video_id)
     if not transcript:
-        return jsonify({"error": "Unable to fetch transcript."}), 404
+        return jsonify({"message": "Unable to fetch transcript."}), 404
 
     return jsonify(transcript)
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    response = jsonify({"message": str(e)})
+    response.status_code = 500
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
